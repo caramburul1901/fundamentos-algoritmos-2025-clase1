0ece5f11103428ed0311e8e3126ca14c92ca7efd
caramburul <claudio.aramburu.l@gmail.com>
2025-08-19 00:04:25 -0500

    chore: solución base con C# y Python

---
+++ Clase1_1/HolaMundoPy/HolaMundoPy.py
@@ -0,0 +1,1 @@
+print ("Hola, fundamentos de algoritmos - Python")
---
+++ Clase1_1/HolaMundocs/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hola, Fundamentos de Algoritmos 20252— C#");
+    }
+}
