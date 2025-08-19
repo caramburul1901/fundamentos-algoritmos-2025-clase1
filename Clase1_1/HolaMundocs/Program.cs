c19ed56a07cd3c005dcad623e2c4afa0ab49eeca
caramburul <claudio.aramburu.l@gmail.com>
2025-08-19 00:13:17 -0500

    Nueva Rama

--- Clase1_1/HolaMundocs/Program.cs
+++ Clase1_1/HolaMundocs/Program.cs
@@ -3,6 +3,6 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hola, Fundamentos de Algoritmos 20252— C#");
+        Console.WriteLine("Hola, Fundamentos de Algoritmos 20252— C#-Nueva Rama");
     }
 }
